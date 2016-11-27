195dcb898567b9c7ebb380b7a555ccadd761e67c
Surajit Basak <surajitbasak109@gmail.com>
2016-11-28 03:26:04 +0530

    Updated 1

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
-$(document).ready(function () {
-    'use strict';
-    $("a.page-scroll").bind("click", function (a) {
+$(document).ready(function() {
+    $("a.page-scroll").bind("click", function(a) {
         var target = $(this);
         $("html, body").stop().animate({
             scrollTop: $(target.attr("href")).offset().top - 50
-        }, 1250, "easeInOutExpo"), a.preventDefault();
-    }), $("body").scrollspy({
-        target: ".navbar-fixed-top"
-        , offset: 100
-    }), $(".navbar-collapse ul li a").click(function () {
+        }, 1250, "easeInOutExpo"),
+        a.preventDefault();
+    }),
+    $("body").scrollspy({
+        target: ".navbar-fixed-top",
+        offset: 100
+    }),
+    $(".navbar-collapse ul li a").click(function() {
         $(".navbar-toggle:visible").click()
-    }), $("#topNav").affix({
+    }),
+    $("#topNav").affix({
         offset: {
             top: 50
         }
-    })
+    });
 });
